e77cac3e94e119d9d7647bc2af255c0a85109a70
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-08 17:08:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def new_hash
-  # return an empty hash
+  new_hash = Hash.new 
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  My_hash = {"Ricardo" => 30, "Taryn" => 29, "Gizmo" => 8, "Austin" => 1}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {name: "Grace Hopper"}
 end
 
 def id_generator
